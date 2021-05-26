d8e8fa7b60aa4bacf69faf36ff9f35cdd46c9dc0
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-26 22:16:47 +0530

    Add : winning count of head and tail

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,9 +1,21 @@
 #!/bin/bash -x
 isHead=1
+i=0
+j=0
+read -p "Enter a number that how many times you want to flip a coin : " num
+flip=0
+while [[ $flip < $num ]]
+do
 randomCheck=$((RANDOM%2))
-if [ $randomCheck -eq $isPrime ]
+if [[ $randomCheck -eq $isPrime ]]
 then
 	echo "Head is the winner"
+	i=$(($i+1))
 else
 	echo "Tail is the winner"
+	j=$(($j+1))
 fi
+flip=$(($flip+1))
+done
+echo "Head has won $i times"
+echo "Tail has won $j times"
