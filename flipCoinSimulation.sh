ec9ee63545857bb119da7e83049eb4a5fa00634e
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-26 22:32:27 +0530

    Add : Multiple flips for winning of head or tail for 21 times

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -2,20 +2,33 @@
 isHead=1
 i=0
 j=0
-read -p "Enter a number that how many times you want to flip a coin : " num
+read -p "Enter how many times you want to flip your coin : " num
 flip=0
-while [[ $flip < $num ]]
+while [ $flip -lt $num ]
 do
-randomCheck=$((RANDOM%2))
-if [[ $randomCheck -eq $isPrime ]]
-then
-	echo "Head is the winner"
-	i=$(($i+1))
-else
-	echo "Tail is the winner"
-	j=$(($j+1))
-fi
-flip=$(($flip+1))
+        randomCheck=$((RANDOM%2))
+        if [[ $randomCheck -eq $isHead ]]
+        then
+                echo "Head is the winner"
+                i=$(($i+1))
+        else
+                echo "Tail is the winner"
+                j=$(($j+1))
+        fi
+        if [[ $i -eq 21 && $j -eq 21  ]]
+        then
+                echo "Tie"
+                break
+        elif [[ $i -eq 21 ]]
+        then
+                echo "Head has won"
+                break
+        elif [[ $j -eq 21 ]]
+        then
+                echo "Tail has won"
+                break
+        fi
+        flip=$(($flip+1))
 done
 echo "Head has won $i times"
 echo "Tail has won $j times"
